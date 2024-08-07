8b26475f994dd8b253d077aa4bd1446694ac6231
Amin Chen <minde85731@gmail.com>
2024-08-07 14:38:25 +0800

    完成基本選擇表單

--- MidtermProject/Form1.Designer.cs
+++ MidtermProject/Form1.Designer.cs
@@ -29,23 +29,57 @@
 		private void InitializeComponent()
 		{
 			this.buttonConnDb = new System.Windows.Forms.Button();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.buttonGetAll = new System.Windows.Forms.Button();
+			this.buttonMbships = new System.Windows.Forms.Button();
 			this.SuspendLayout();
 			// 
 			// buttonConnDb
 			// 
 			this.buttonConnDb.Location = new System.Drawing.Point(27, 28);
 			this.buttonConnDb.Name = "buttonConnDb";
-			this.buttonConnDb.Size = new System.Drawing.Size(204, 64);
+			this.buttonConnDb.Size = new System.Drawing.Size(204, 40);
 			this.buttonConnDb.TabIndex = 0;
 			this.buttonConnDb.Text = "連結Db";
 			this.buttonConnDb.UseVisualStyleBackColor = true;
 			this.buttonConnDb.Click += new System.EventHandler(this.buttonConnDb_Click);
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(27, 83);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(123, 20);
+			this.comboBox1.TabIndex = 1;
+			// 
+			// buttonGetAll
+			// 
+			this.buttonGetAll.Location = new System.Drawing.Point(156, 80);
+			this.buttonGetAll.Name = "buttonGetAll";
+			this.buttonGetAll.Size = new System.Drawing.Size(75, 23);
+			this.buttonGetAll.TabIndex = 2;
+			this.buttonGetAll.Text = "清單總覽";
+			this.buttonGetAll.UseVisualStyleBackColor = true;
+			this.buttonGetAll.Click += new System.EventHandler(this.buttonGetAll_Click);
+			// 
+			// buttonMbships
+			// 
+			this.buttonMbships.Location = new System.Drawing.Point(27, 126);
+			this.buttonMbships.Name = "buttonMbships";
+			this.buttonMbships.Size = new System.Drawing.Size(123, 54);
+			this.buttonMbships.TabIndex = 3;
+			this.buttonMbships.Text = "會員資格編輯";
+			this.buttonMbships.UseVisualStyleBackColor = true;
+			this.buttonMbships.Click += new System.EventHandler(this.buttonMbships_Click);
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.buttonMbships);
+			this.Controls.Add(this.buttonGetAll);
+			this.Controls.Add(this.comboBox1);
 			this.Controls.Add(this.buttonConnDb);
 			this.Name = "Form1";
 			this.Text = "Form1";
@@ -56,6 +90,9 @@
 		#endregion
 
 		private System.Windows.Forms.Button buttonConnDb;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Button buttonGetAll;
+		private System.Windows.Forms.Button buttonMbships;
 	}
 }
 
--- MidtermProject/Form1.cs
+++ MidtermProject/Form1.cs
@@ -16,6 +16,7 @@ namespace MidtermProject
 		public Form1()
 		{
 			InitializeComponent();
+			BindData();
 		}
 
 		private void buttonConnDb_Click(object sender, EventArgs e)
@@ -26,5 +27,60 @@ namespace MidtermProject
 			}
 			MessageBox.Show("連結成功");
 		}
+
+		public void BindData()
+		{
+			using (var conn = new SqlDb().Getconn("default"))
+			{
+				try
+				{
+					conn.Open();
+					DataTable schema = conn.GetSchema("Tables");
+
+					var tablesHide = new[] { "sysdiagrams", "Admins" };
+
+					foreach(DataRow row in schema.Rows)
+					{
+						string tableName = row["TABLE_NAME"].ToString();
+
+						if (!tablesHide.Contains(tableName))
+						{
+							comboBox1.Items.Add(tableName);
+						}
+						
+					}
+				}
+				catch(Exception ex)
+				{
+					MessageBox.Show("錯誤: " + ex.Message);
+				}
+
+			}
+		}
+
+		private void buttonMbships_Click(object sender, EventArgs e)
+		{
+			new FormMbships().ShowDialog();
+		}
+
+		private void buttonGetAll_Click(object sender, EventArgs e)
+		{			
+
+			int selected = comboBox1.SelectedIndex;
+			
+			switch (selected)
+			{
+				case -1:
+					MessageBox.Show("未選取資料表"); ;
+					break;
+				case 1:
+					new FormMbships().ShowDialog();
+					break;
+				default:
+					MessageBox.Show("未找到資料表");
+					return;
+			}
+		}			
+
 	}
 }
